c4ef1bd2c175adc9d26f401dbb5c634b29a3053b
jamescfy <49020839+jamescfy@users.noreply.github.com>
2019-04-16 10:30:46 +0800

    添加熔断机制

--- son-api/src/main/java/com/trust/one/entities/Dept.java
+++ son-api/src/main/java/com/trust/one/entities/Dept.java
@@ -2,13 +2,17 @@ package com.trust.one.entities;
 
 import java.io.Serializable;
 
+import javax.jws.HandlerChain;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Accessors(chain = true)
 public class Dept implements Serializable{
 	
     private static final long serialVersionUID =1L;
