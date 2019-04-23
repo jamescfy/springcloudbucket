7e93cd9c429da89d68dbb735fe88f722f1b26a11
jamescfy <49020839+jamescfy@users.noreply.github.com>
2019-04-23 09:47:05 +0800

    add spring cloud config

---
+++ son-config-client-3355/src/main/java/com/trust/one/ConfigClientApplication_3355.java
@@ -0,0 +1,13 @@
+package com.trust.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication_3355 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication_3355.class, args);
+	}
+
+}
